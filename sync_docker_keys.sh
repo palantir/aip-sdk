155843ae30ab12236ab35e24e88f7500426d6c76
Mary Cameron <mcameron@palantir.com>
2021-11-19 12:51:08 -0500

    Revert "Merge pull request #20 from mary32/mac-quickstart-updates"
    This reverts commit d75d0c4b1f65eadf195547bc74f788d80ccafd1b, reversing
    changes made to 3df02be9fd543a4e55a16589fce2577b3401ddca.

--- sync_docker_keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# sync in things you need for certs
-rsync -a /etc/apt/ apt/
-rsync -a /usr/share/keyrings/ keyrings/
-rsync -a /etc/ssl/ ssl/
-
-# Remove the hosts local repos that doesn't exist in the container
-grep -H -o file: apt/sources.list.d/* | cut -d: -f1 | xargs rm -f
